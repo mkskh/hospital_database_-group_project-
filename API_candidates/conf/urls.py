eb435481a3aca012610e51b290ff59a203435a0e
mkskh <maksym.kharchenko@dci-student.org>
2024-03-15 12:08:45 +0100

    created api

--- API_candidates/conf/urls.py
+++ API_candidates/conf/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from candidates.views import redirect_home
 
 urlpatterns = [
+    path('', redirect_home),
     path('admin/', admin.site.urls),
+    path('candidates/', include('candidates.urls', namespace="candidates")),
+
 ]
