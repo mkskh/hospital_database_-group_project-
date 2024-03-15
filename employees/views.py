319083f18a88d51b6414ec5ae0d87a75edcfa7bc
jgohl-dci <johannes.gohl@dci-student.org>
2024-03-15 16:09:51 +0100

    add notes

--- employees/views.py
+++ employees/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .consume_api import get_data
 
 
+# TODO add "gender" and "university"
+# TODO creating proper form to display data
+# TODO create categories like "profession" and "gender"
+# TODO implement filter options
+# TODO fetch potential errors (HTTPError, timeout, to many requests)
+# TODO implement sorting of data (name, age, profession)
+
 def index(request):
     return_data = get_data()
     print("API RETRIEVED", len(return_data))
