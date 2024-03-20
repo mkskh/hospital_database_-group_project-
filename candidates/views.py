bdeb834f1f36ab92494565b03a089d58c121b292
mkskh <maksym.kharchenko@dci-student.org>
2024-03-20 15:26:32 +0100

    made table

--- candidates/views.py
+++ candidates/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'candidates/index.html')
+
+    if request.method == "GET":
+        url = "http://127.0.0.1:8080/candidates/"
+
+        response = requests.get(url).json()
+        
+        example = {"one": "two"}
+
+        return render(request, 'candidates/index.html', {"response": response})
