0237bbb6caee331a66dd2a4d6367088f50a94c00
mkskh <maksym.kharchenko@dci-student.org>
2024-03-21 01:43:23 +0100

    search system completely done

--- candidates/forms.py
+++ candidates/forms.py
@@ -2,4 +2,14 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search = forms.CharField()
+
+    GENDER_CHOICES = (
+        ('', 'Select Gender'),
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+    )
+
+    search = forms.CharField(required=False)
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.Select, required=False)
+    age_more_than = forms.IntegerField(required=False)
+    age_less_than = forms.IntegerField(required=False)
--- candidates/views.py
+++ candidates/views.py
@@ -1,51 +1,48 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
 import requests
-
 from .forms import SearchForm
-
 
 def index(request):
 
+    url = "http://127.0.0.1:8080/candidates/"
+    form = SearchForm(request.POST or None)
+
     if request.method == "GET":
-
-        url = "http://127.0.0.1:8080/candidates/"
-        form = SearchForm()
-
         response = requests.get(url).json()
-
         return render(request, 'candidates/index.html', {"response": response, "form": form})
     
+    if request.method == "POST" and form.is_valid():
+        response = requests.get(url).json()
+        key = form.cleaned_data['search']
+        gender = form.cleaned_data['gender']
+        age_more_than = form.cleaned_data['age_more_than']
+        age_less_than = form.cleaned_data['age_less_than']
 
-    elif request.method == "POST":
+        search_res = []
 
-        url = "http://127.0.0.1:8080/candidates/"
-        form = SearchForm(request.POST)
+        for item in response:
+            if not key:
+                search_res.append(item)
+            else:
+                match_found = False
+                for field in item.values():
+                    if str(key).lower() in str(field).lower():
+                        search_res.append(item)
+                        match_found = True
+                        break
+                if match_found:
+                    continue 
 
-        if form.is_valid():
-            response = requests.get(url).json()
-            key = form.cleaned_data['search']
+        if gender:
+            search_res = [item for item in search_res if item["gender"] == gender]
 
-            search_res = []
+        if age_more_than:
+            search_res = [item for item in search_res if item["age"] > age_more_than]
 
-            if key:
-                for item in response:
-                    if key.lower() in item["first_name"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["last_name"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["city"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["email"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["phone"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["university"].lower():
-                        search_res.append(item)
-                    elif key.lower() in item["profession"].lower():
-                        search_res.append(item)
-                    
-            return render(request, 'candidates/index.html', {"response": search_res, "form": form})
+        if age_less_than:
+            search_res = [item for item in search_res if item["age"] < age_less_than]
+
+        return render(request, 'candidates/index.html', {"response": search_res, "form": form})
 
 
 def reset(request):
